c6c921ac182ef6bcd65d0686e5bb12b8a0be60bc
Mateus Pinheiro <mateus.mcpa@gmail.com>
2024-11-24 08:24:46 -0300

    Desafios 2 e 3 ok. incluído looping do menu de desafios

--- desafio_02.py
+++ desafio_02.py
@@ -20,11 +20,35 @@ def desafio01():
 
 #02. Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
 def desafio02():
-    return None
+    print("Neste desafio, mostrarei o resto da divisão por 5 do número inteiro que você indicar")
+    while True:
+        try:
+            n1 = int(input("Digite um número inteiro: "))
+        except ValueError:
+            continue
+        else:
+            break
+    return n1 % 5
 
 #03. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
 def desafio03():
-    return None
+    print("Neste desafio, multiplicarei 2 números reais para você")
+    while True:
+        try:
+            n1 = float(input("Digite o primeiro número real: "))
+        except ValueError:
+            continue
+        else:
+            break
+    while True:
+        try:
+            n2 = float(input("Digite o segundo número real: "))
+        except ValueError:
+            continue
+        else:
+            break
+    
+    return n1 * n2
 
 #04. Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
 def desafio04():
@@ -95,7 +119,8 @@ def desafio20():
     return None
 
 
-menu = """#01. Escreva um programa que soma dois números inteiros inseridos pelo usuário.
+menu = """MENU DE DESAFIOS
+#01. Escreva um programa que soma dois números inteiros inseridos pelo usuário.
 #02. Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
 #03. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
 #04. Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
@@ -116,48 +141,53 @@ menu = """#01. Escreva um programa que soma dois números inteiros inseridos pel
 #19. Faça um programa que compare se dois números fornecidos pelo usuário são iguais.
 #20. Escreva um programa que verifique se dois números fornecidos pelo usuário são diferentes."""
 
-desafio = None
-print(menu)
+
 
 
 def main():
-    desafio = None
-    while desafio not in range(1,21):
-        try:
-            desafio = int(input("Escolha o desafio de 1 a 20: \n"))
-        except ValueError:
-            continue
-    
-    print("Você escolheu o desafio ",desafio,"!")
+    while True:
+        print(menu)
+        desafio = None
+        while desafio not in range(1,21):
+            try:
+                desafio = int(input("Escolha o desafio de 1 a 20: \n"))
+            except ValueError:
+                continue
+        
+        print("Você escolheu o desafio ",desafio,"!")
 
-    desafios = {
-    1: desafio01,
-    2: desafio02,
-    3: desafio03,
-    4: desafio04,
-    5: desafio05,
-    6: desafio06,
-    7: desafio07,
-    8: desafio08,
-    9: desafio09,
-    10: desafio10,
-    11: desafio11,
-    12: desafio12,
-    13: desafio13,
-    14: desafio14,
-    15: desafio15,
-    16: desafio16,
-    17: desafio17,
-    18: desafio18,
-    19: desafio19,
-    20: desafio20}
+        desafios = {
+        1: desafio01,
+        2: desafio02,
+        3: desafio03,
+        4: desafio04,
+        5: desafio05,
+        6: desafio06,
+        7: desafio07,
+        8: desafio08,
+        9: desafio09,
+        10: desafio10,
+        11: desafio11,
+        12: desafio12,
+        13: desafio13,
+        14: desafio14,
+        15: desafio15,
+        16: desafio16,
+        17: desafio17,
+        18: desafio18,
+        19: desafio19,
+        20: desafio20}
 
-    resultado = desafios[desafio]()
+        resultado = desafios[desafio]()
 
-    print(4*"~=~","RESULTADO",4*"~=~")
-    print(f'{resultado:^33}')
-    print(7*"~=~=~")
-
+        print(4*"~=~","RESULTADO",4*"~=~")
+        print(f'{resultado:^33}')
+        print(7*"~=~=~")
+        resposta = None
+        while resposta not in ['y','yes','yep','n','not','nope']:
+            resposta = input('Gostaria de abrir outro desafio? [y/n]: ')
+            if resposta in ['n','nope','not']:
+                break
 
 if __name__ == "__main__":
     main()
