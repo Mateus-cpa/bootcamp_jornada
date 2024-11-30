fa8ae8ee7bc2619672962d76741216238c96dc2d
Mateus Pinheiro <mateus.mcpa@gmail.com>
2024-11-30 08:08:50 -0300

    Desafio 2 complmeentado com as instruções até o desafio 40. Falta fazer o código a partir do 26.

--- desafio_02.py
+++ desafio_02.py
@@ -422,6 +422,105 @@ def desafio25():
     else:
         return lista_separada
 
+#Exercício 26: Verificação de Qualidade de Dados
+#Você está analisando um conjunto de dados de vendas e precisa garantir que 
+#todos os registros tenham valores positivos para quantidade e preço. 
+#Escreva um programa que verifique esses campos e imprima "Dados válidos" 
+#se ambos forem positivos ou "Dados inválidos" caso contrário.
+def desafio26():
+    return None
+
+#Exercício 27: Classificação de Dados de Sensor
+#Imagine que você está trabalhando com dados de sensores IoT. 
+#Os dados incluem medições de temperatura. 
+#Você precisa classificar cada leitura como 'Baixa', 'Normal' ou 'Alta'. Considerando que:
+#Temperatura < 18°C é 'Baixa'
+#Temperatura >= 18°C e <= 26°C é 'Normal'
+#Temperatura > 26°C é 'Alta'
+def desafio27():
+    return None
+
+#Exercício 28: Filtragem de Logs por Severidade
+#Você está analisando logs de uma aplicação e precisa filtrar mensagens com severidade 'ERROR'. 
+# Dado um registro de log em formato de dicionário como 
+# log = {'timestamp': '2021-06-23 10:00:00', 'level': 'ERROR', 'message': 'Falha na conexão'}, 
+# escreva um programa que imprima a mensagem se a severidade for 'ERROR'.
+def desafio28():
+    return None
+
+#Exercício 29: Validação de Dados de Entrada
+#Antes de processar os dados de usuários em um sistema de recomendação, 
+# você precisa garantir que cada usuário tenha idade entre 18 e 65 anos e tenha fornecido um email válido. 
+# Escreva um programa que valide essas condições e imprima "Dados de usuário válidos" 
+# ou o erro específico encontrado.
+def desafio29():
+    return None
+
+#Exercício 30: Detecção de Anomalias em Dados de Transações
+#Você está trabalhando em um sistema de detecção de fraude e precisa identificar transações suspeitas.
+#Uma transação é considerada suspeita se o valor for superior a R$ 10.000 ou se ocorrer fora do horário 
+# comercial (antes das 9h ou depois das 18h). Dada uma transação como 
+# transacao = {'valor': 12000, 'hora': 20}, verifique se ela é suspeita.
+def desafio30():
+    return None
+
+#Exercício 31: Contagem de Palavras em Textos
+# Dado um texto, contar quantas vezes cada palavra única aparece nele.
+def desafio31():
+    return None
+
+
+#Exercício 32. Normalização de Dados
+#Normalizar uma lista de números para que fiquem na escala de 0 a 1.
+def desafio32():
+    return None
+
+
+#Exercício 33. Filtragem de Dados Faltantes
+#Dada uma lista de dicionários representando dados de usuários, 
+# filtrar aqueles que têm um campo específico faltando.
+def desafio33():
+    return None
+
+
+#Exercício 34. Extração de Subconjuntos de Dados
+# Dada uma lista de números, extrair apenas aqueles que são pares.
+def desafio34():
+    return None
+
+
+#Exercício 35. Agregação de Dados por Categoria
+#Dado um conjunto de registros de vendas, calcular o total de vendas por categoria.
+def desafio35():
+    return None
+
+#Exercício 36. Leitura de Dados até Flag
+#Ler dados de entrada até que uma palavra-chave específica ("sair") seja fornecida.
+def desafio36():
+    return None
+
+#Exercício 37. Validação de Entrada
+#Solicitar ao usuário um número dentro de um intervalo específico até que a entrada seja válida.
+def desafio37():
+    return None
+
+
+#Exercício 38. Consumo de API Simulado
+#Simular o consumo de uma API paginada, 
+# onde cada "página" de dados é processada em loop até que não haja mais páginas.
+def desafio38():
+    return None
+
+#Exercício  39. Tentativas de Conexão
+#Simular tentativas de reconexão a um serviço com um limite máximo de tentativas.
+def desafio39():
+    return None
+
+#Exercício 40. Processamento de Dados com Condição de Parada
+#Processar itens de uma lista até encontrar um valor específico que indica a parada.
+def desafio40():
+    return None
+
 menu = """MENU DE DESAFIOS
 #01. Escreva um programa que soma dois números inteiros inseridos pelo usuário.
 #02. Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
@@ -448,6 +547,22 @@ menu = """MENU DE DESAFIOS
 #23. Faça uma calculadora simples.
 #24. Classifique um número intenro em (positivo/negativo/neutro) e (par/ímpar).
 #25. Converta uma lista de números de string para lista.
+#26. Verificação de Qualidade de Dados
+#27. Classificação de Dados de Sensor
+#28. Filtragem de Logs por Severidade
+#29: Validação de Dados de Entrada
+#30: Detecção de Anomalias em Dados de Transações
+#31: Contagem de Palavras em Textos
+#32. Normalização de Dados
+#33. Filtragem de Dados Faltantes
+#34. Extração de Subconjuntos de Dados
+#35. Agregação de Dados por Categoria
+#36. Leitura de Dados até Flag
+#37. Validação de Entrada
+#38. Consumo de API Simulado
+#39. Tentativas de Conexão
+#40. Processamento de Dados com Condição de Parada
+
 """
 
 
@@ -491,7 +606,22 @@ def main():
         22: desafio22,
         23: desafio23,
         24: desafio24,
-        25: desafio25}
+        25: desafio25,
+        26: desafio26,
+        27: desafio27,
+        28: desafio28,
+        29: desafio29,
+        30: desafio30,
+        31: desafio31,
+        32: desafio32,
+        33: desafio33,
+        34: desafio34,
+        35: desafio35,
+        36: desafio36,
+        37: desafio37,
+        38: desafio38,
+        39: desafio39,
+        40: desafio40}
         
 
 
