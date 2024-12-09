9002d26c28375c04153427519721f1ec9ed0335e
Mateus Pinheiro <mateus.mcpa@gmail.com>
2024-12-09 12:18:01 -0300

    :construction: feat: enviadno código desafio_03.py

---
+++ desafio_03.py
@@ -0,0 +1,13 @@
+#Crie um dicionário para armazenar informações de um livro,
+# incluindo Título, autor e ano de publicação. Imprima cada informação
+
+from typing import Dict, Any
+
+livro:Dict[str, Any] = {
+    "Título": "Bíblia",
+    "Autor": "Vários",
+    "Ano": -4000
+}
+
+print(livro)
+
